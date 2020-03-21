09caf6717135ab3bf064548a6b99371e7ee76516
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2020-03-21 13:13:31 +0000

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+
+const sampleText = document.querySelector('.main-content__sample-text');
+const userInputText = document.querySelector('.main-content__user-text');
+const fullText = sampleText.innerHTML;
+const correctSpanStartTag = '<span class="correct">';
+
+let index = 0;
+
+
+function spanAround(spanDefinition){
+    return function (string,start,end){
+        return string.slice(0,start) + spanDefinition + string.slice(start,end) + '</span>' + string.slice(end,string.length);
+    };
+}
+
+function keyPressHandler(e) {
+    let characterPressed = e.key;
+    let keyCode = e.keyCode;
+    console.log("Expected: " + fullText[index]);
+    console.log("Pressed: " + characterPressed)
+    function checkMatchesNext(characterPressed,keyCode){
+        if (characterPressed == fullText[index]){
+            index++;
+            sampleText.innerHTML = spanCorrect(fullText,index);
+            if (keyCode == 32){
+                userInputText.value = '';
+            }
+        }
+        
+    }
+    return checkMatchesNext(characterPressed,keyCode);
+}
+
+
+spanCorrect = function (fullText,end) {
+    return spanAround(correctSpanStartTag)(fullText,0,end); 
+};
+
+
+userInputText.addEventListener('keypress',keyPressHandler,false);

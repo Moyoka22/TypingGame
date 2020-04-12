c3b50aee90e949bdbe59ad4f6e25106185aa8d60
Moyewa Odiase <moyeodiase@hotmail.co.uk>
2020-04-12 11:59:28 +0100

    Refactored code to use singleton timer

--- scripts/main.js
+++ scripts/main.js
@@ -1,220 +1,260 @@
-
-const sampleText = document.querySelector('.main-content__sample-text');
-const userInputText = document.querySelector('.main-content__user-text');
-const fullText = sampleText.innerHTML; // TODO : Retrieve a random text file from some repository of sample texts 
+const sampleText = document.getElementById("sample-text");
+const userInputText = document.getElementById("user-text");
+const fullText = sampleText.innerHTML; // TODO : Retrieve a random text file from some repository of sample texts
 
 const correctSpanStartTag = '<span class="correct">';
 const incorrectSpanStartTag = '<span class="incorrect">';
+const cursorSpanStartTag = '<span class="incorrect">';
 
-spanCorrect = function (fullText, end) {
-    return spanAround(correctSpanStartTag)(fullText, 0, end) + fullText.slice(end, fullText.length);
-};
-
-spanCorrectIncorect = function (fullText, endCorrect, endIncorrect) {
-    return spanAround(correctSpanStartTag)(fullText, 0, endCorrect) + spanAround(incorrectSpanStartTag)(fullText, endCorrect, endIncorrect) + fullText.slice(endIncorrect, fullText.length);
-};
-
-const metricsSection = document.querySelector('.metrics');
-const metricsWPMCount = metricsSection.querySelector('.metrics__wpm-count');
-const metricsCPMCount = metricsSection.querySelector('.metrics__cpm-count');
-const metricsAccuracy = metricsSection.querySelector('.metrics__accuracy-percentage');
-const metricsReset = metricsSection.querySelector('.metrics__reset-btn');
-
-const refreshRate = 50;
-const updateSteps = 10;
-
-class Metric {
-    constructor() {
-        this._charsTyped = 0;
-        this._wordsTyped = 0;
-        this._correctCharsTyped = 0;
-        this.getAccuracy = function () {
-            if (this._charsTyped == 0) {
-                return 0;
-            }
-            return this._correctCharsTyped / this._charsTyped;
-        };
-        this.getCPM = function (seconds) {
-            return this._correctCharsTyped * 60 / seconds;
-        };
-        this.getWPM = function (seconds) {
-            return this._wordsTyped * 60 / seconds;
-        };
-        this.incrementCorrect = function () {
-            this._charsTyped++;
-            this._correctCharsTyped++;
-        };
-        this.incrementIncorrect = function () {
-            this._charsTyped++;
-        };
-        this.getWordsTyped = function () {
-            return this._wordsTyped;
-        };
-        this.incrementWord = function () {
-            this._wordsTyped++;
-        };
-    }
+function spanAround(spanDefinition) {
+  return function (string, start, end) {
+    return spanDefinition + string.slice(start, end) + "</span>";
+  };
 }
 
-
-let attemptFactory = function (sampleText) {
-    return (function () {
-        let index = 0;
-        let correctIndex = 0;
-        let wordBoundary = 0;
-        let numOfWords = sampleText.split(' ').length;
-        let met = new Metric();
-        return {
-            nextChar: function () {
-                return sampleText[correctIndex];
-            },
-            incrementCorrect: function () {
-                index++;
-                correctIndex++;
-                met.incrementCorrect();
-            },
-            incrementIncorrect: function () {
-                index++;
-                met.incrementIncorrect();
-            },
-            isWordBoundary() {
-                return correctIndex >= 1 && sampleText[correctIndex - 1] == ' ';
-            },
-            incrementWord() {
-                met.incrementWord();
-                wordBoundary = index;
-            },
-            checkChar(char) {
-
-                if (correctIndex != index) {
-                    return false;
-                }
-                if (char == this.nextChar()) {
-                    this.incrementCorrect();
-                    if (this.isWordBoundary() || sampleText.length == correctIndex) {
-                        this.incrementWord();
-                    }
-                    return true;
-                } else {
-                    this.incrementIncorrect();
-                    return false;
-                }
-
-
-            },
-            isFinished() {
-                return met.getWordsTyped() == numOfWords;
-            },
-            getIndex() {
-                return index;
-            },
-            getCorrectIndex() {
-                return correctIndex;
-            },
-            backspace() {
-                if (index > correctIndex) {
-                    index--;
-                }
-            },
-            getWPM(runningTime) {
-                return met.getWPM(runningTime);
-            },
-            getCPM(runningTime) {
-                return met.getCPM(runningTime);
-            },
-            getAccuracy() {
-                return met.getAccuracy();
-            }
-        };
-    })();
+const spanCorrect = (fullText, end) => {
+  return (
+    spanAround(correctSpanStartTag)(fullText, 0, end - 1) +
+    fullText.slice(end, fullText.length)
+  );
 };
+
+const spanCorrectIncorrect = (fullText, endCorrect, endIncorrect) => {
+  return (
+    spanAround(correctSpanStartTag)(fullText, 0, endCorrect) +
+    spanAround(incorrectSpanStartTag)(fullText, endCorrect, endIncorrect) +
+    fullText.slice(endIncorrect, fullText.length)
+  );
+};
+
+const updateSampleText = (sampleTextElement, fullText, index, correctIndex) => {
+  sampleTextElement.innerHTML =
+    index == correctIndex
+      ? spanCorrect(fullText, index)
+      : spanCorrectIncorect(fullText, index, endIncorrect);
+};
+
+const metricsSection = document.getElementById("metrics");
+const metricsWPMCount = metricsSection.querySelector("#wpm-count");
+const metricsCPMCount = metricsSection.querySelector("#cpm-count");
+const metricsAccuracy = metricsSection.querySelector("#accuracy-percentage");
+const metricsReset = metricsSection.querySelector("#reset-btn");
+
+class Metric {
+  constructor() {
+    this._charsTyped = 0;
+    this._wordsTyped = 0;
+    this._correctCharsTyped = 0;
+    this.getAccuracy = function () {
+      if (this._charsTyped == 0) {
+        return 0;
+      }
+      return this._correctCharsTyped / this._charsTyped;
+    };
+    this.getCPM = function (seconds) {
+      return (this._correctCharsTyped * 60) / seconds;
+    };
+    this.getWPM = function (seconds) {
+      return (this._wordsTyped * 60) / seconds;
+    };
+    this.incrementCorrect = function () {
+      this._charsTyped++;
+      this._correctCharsTyped++;
+    };
+    this.incrementIncorrect = function () {
+      this._charsTyped++;
+    };
+    this.getWordsTyped = function () {
+      return this._wordsTyped;
+    };
+    this.incrementWord = function () {
+      this._wordsTyped++;
+    };
+  }
+}
+
+class Attempt {
+  constructor(sampleText) {
+    this._index = 0;
+    this._correctIndex = 0;
+    this._wordBoundary = 0;
+    this._numOfWords = sampleText.split(" ").length;
+    this.met = new Metric();
+    this.nextChar = () => {
+      return sampleText[this._correctIndex];
+    };
+    this.incrementCorrect = () => {
+      this._index++;
+      this._correctIndex++;
+      this.met.incrementCorrect();
+    };
+    this.incrementIncorrect = () => {
+      this._index++;
+      this.met.incrementIncorrect();
+    };
+    this.isWordBoundary = () => {
+      return (
+        this._correctIndex >= 1 && sampleText[this._correctIndex - 1] == " "
+      );
+    };
+    this.incrementWord = () => {
+      this.met.incrementWord();
+      this._wordBoundary = this._index;
+    };
+    this.checkChar = (char) => {
+      if (this._correctIndex != this._index) {
+        return false;
+      }
+      if (char == this.nextChar()) {
+        this.incrementCorrect();
+        if (this.isWordBoundary() || sampleText.length == this._correctIndex) {
+          this.incrementWord();
+        }
+        return true;
+      } else {
+        this.incrementIncorrect();
+        return false;
+      }
+    };
+
+    this.isFinished = () => {
+      return this.met.getWordsTyped() == this._numOfWords;
+    };
+    this.getIndex = () => {
+      return this._index;
+    };
+    this.getMets = (runningTime) => {
+      return {
+        WPM: this.met.getWPM(runningTime),
+        CPM: this.met.getCPM(runningTime),
+        Accuracy: this.met.getAccuracy(),
+      };
+    };
+    this.backspace = () => {
+      if (this._index > this._correctIndex) {
+        this._index--;
+      }
+    };
+    this.getCorrectIndex = () => {
+      return this._correctIndex;
+    };
+  }
+}
 
 let currentAttempt = null;
 let pastAttempts = [];
 
-// TODO : Timer Object
-let runningTime = 0;
-let timerID = null;
-let timerRunning = false;
+const refreshMetric = (force) => {
+  const t = timer.getInstance();
+  t.incrementRunningTime();
+  const runningTime = t.getRunningTime();
+  const updateSteps = 10;
+  steps = t.getSteps();
+  if (steps % updateSteps == 0 || force) {
+    let mets = currentAttempt.getMets(runningTime);
+    metricsWPMCount.innerHTML = mets.WPM.toFixed(2);
+    metricsCPMCount.innerHTML = mets.CPM.toFixed(2);
+    metricsAccuracy.innerHTML = (mets.Accuracy * 100).toFixed(2) + "%";
+  }
+};
 
+let timer = (() => {
+  let _instance;
+  let runningTime = 0;
+  let timerRunning = false;
+  let timerID = null;
+  const refreshRate = 50;
 
-function refresh(force) {
-    runningTime += (refreshRate / 1000);
-    steps = (runningTime * 1000 / refreshRate) >> 0;
-    if (steps % updateSteps == 0 || force) {
-        metricsWPMCount.innerHTML = currentAttempt.getWPM(runningTime).toFixed(2);
-        metricsCPMCount.innerHTML = currentAttempt.getCPM(runningTime).toFixed(2);
-        metricsAccuracy.innerHTML = (currentAttempt.getAccuracy() * 100).toFixed(2) + '%';
-    }
-}
-
-function resetSampleText() {
-    sampleText.innerHTML = fullText;
-}
-function startTimer() {
-    timerRunning = true;
-    metricsReset.classList.toggle('metrics__reset--active');
-    return setInterval(refresh, refreshRate);
-}
-
-function stopTimer() {
-    if (timerRunning) {
-        clearInterval(timerID);
+  let createInstance = () => {
+    return {
+      getSteps: () => {
+        return ((runningTime * 1000) / refreshRate) >> 0;
+      },
+      getRunningTime: () => {
+        return runningTime;
+      },
+      incrementRunningTime: () => {
+        runningTime += refreshRate / 1000;
+      },
+      getTimerID: () => {
+        return timerID;
+      },
+      isRunning: () => {
+        return timerRunning;
+      },
+      stop: () => {
         timerRunning = false;
         runningTime = 0;
-        metricsReset.classList.toggle('metrics__reset--active');
-        setTimeout(function () {
-            userInputText.value = ''
-        }, 0);
-        resetSampleText();
-    }
-}
-
-function spanAround(spanDefinition) {
-    return function (string, start, end) {
-        return spanDefinition + string.slice(start, end) + '</span>';
+        clearInterval(timerID);
+        return timerID;
+      },
+      start: () => {
+        timerRunning = true;
+        timerID = setInterval(refreshMetric, refreshRate);
+        return timerID;
+      },
     };
-}
+  };
 
-function userInputHandler(e, self) {
-    if (!timerRunning) {
-        currentAttempt = attemptFactory(fullText);
-        timerID = startTimer();
+  return {
+    getInstance: () => {
+      return _instance ? _instance : createInstance();
+    },
+  };
+})();
+
+const userInputHandler = (e) => {
+  if (!timer.getInstance().isRunning()) {
+    currentAttempt = new Attempt(fullText);
+    timer.getInstance().start();
+  }
+  let correctCharPressed = currentAttempt.checkChar(e.key);
+  if (correctCharPressed) {
+    if (currentAttempt.isWordBoundary()) {
+      userInputText.value = "";
+    } else if (currentAttempt.isFinished()) {
+      userInputText.value = "";
+      refresh(true);
+      stopTimer();
     }
+  }
+  sampleText.innerHTML = spanCorrectIncorrect(
+    fullText,
+    currentAttempt.getCorrectIndex(),
+    currentAttempt.getIndex()
+  );
+};
 
-    let characterPressed = e.key;
-    let correctCharPressed = currentAttempt.checkChar(characterPressed);
-    if (correctCharPressed) {
-        if (currentAttempt.isWordBoundary()) {
-            userInputText.value = '';
-        } else if (currentAttempt.isFinished()) {
-            userInputText.value = '';
-            refresh(true);
-            stopTimer();
+const resetSampleText = () => {
+  sampleText.innerHTML = fullText;
+};
 
-        }
-    }
-    sampleText.innerHTML = spanCorrectIncorect(fullText, currentAttempt.getCorrectIndex(), currentAttempt.getIndex());
-}
+const resetUserInput = () => {
+  userInputText.value = "";
+};
+const keydownHandler = (e) => {
+  let keyCode = e.keyCode;
+  if (
+    (keyCode == 8 || keyCode == 46) &&
+    currentAttempt.getIndex() > currentAttempt.getCorrectIndex()
+  ) {
+    currentAttempt.backspace();
+    sampleText.innerHTML = spanCorrectIncorrect(
+      fullText,
+      currentAttempt.getCorrectIndex(),
+      currentAttempt.getIndex()
+    );
+  }
+};
+const resetButtonHandler = (e) => {
+  e.preventDefault();
+  t = timer.getInstance();
+  t.stop();
+  resetUserInput();
+  resetSampleText();
+};
 
-
-function userKeyDownHandler(e, self) {
-    let keyCode = e.keyCode;
-    if ((keyCode == 8 || keyCode == 46) && currentAttempt.getIndex() > currentAttempt.getCorrectIndex()) {
-        currentAttempt.backspace();
-        sampleText.innerHTML = spanCorrectIncorect(fullText, currentAttempt.getCorrectIndex(), currentAttempt.getIndex());
-    }
-}
-
-
-
-userInputText.addEventListener('keypress', function () {
-    userInputHandler(event, this);
-}, false);
-userInputText.addEventListener('keydown', function () {
-    userKeyDownHandler(event, this);
-}, false);
-
-metricsReset.addEventListener('click', function () {
-    stopTimer();
-}, false);
+// Event Listeners
+userInputText.addEventListener("keydown", keydownHandler, false);
+userInputText.addEventListener("keypress", userInputHandler, false);
+metricsReset.addEventListener("click", resetButtonHandler, false);
